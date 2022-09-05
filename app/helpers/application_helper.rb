7f46b68a1bb1e1ed107dc8526e76827c4cbd1da5
Ahsan-Ali098 <ahsansaleem098@gmail.com>
2022-09-05 13:55:59 +0500

    Rubocop fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
+# Application Helper
+#
 module ApplicationHelper
 end
--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# Application
+#
 require_relative 'boot'
 
 require 'rails/all'
@@ -7,6 +11,8 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module EMS
+  # Application
+  #
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
