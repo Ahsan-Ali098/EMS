ede1fc9a492488a788bfc654c412a021828d3499
Ahsan-Ali098 <ahsansaleem098@gmail.com>
2022-09-07 19:00:10 +0500

    CRUD for Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/edit'
-  get 'users/new'
-  get 'users/show'
   root to: 'home#index'
   devise_for :users
+  namespace :admin do
+    resources :users
+  end
 end
