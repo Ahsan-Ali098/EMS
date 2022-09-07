60a76f998b8130592d1d5d75a06b68666704d6f0
Ahsan-Ali098 <ahsansaleem098@gmail.com>
2022-09-07 15:04:50 +0500

    Created User COntroller and Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/edit'
+  get 'users/new'
+  get 'users/show'
   root to: 'home#index'
   devise_for :users
 end
