9f3f01759fb3fa49ab08a73eaaab31b52fb682e4
Ahsan-Ali098 <ahsansaleem098@gmail.com>
2022-09-09 12:50:36 +0500

    Sorting in Users Listing

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,13 +4,14 @@
 module Admin
   # Class User Controller
   class UsersController < ApplicationController
+    helper_method :sort_column, :sort_direction
     before_action :find_user, only: %i[show update edit destroy]
 
     def index
       @users = if params[:search].present?
                  User.search_user(params[:search]).page(params[:page])
                else
-                 User.all.page(params[:page])
+                 User.all.page(params[:page]).order(sort_column + ' ' + sort_direction).page(params[:page])
                end
     end
 
@@ -52,6 +53,14 @@ module Admin
 
     def user_params
       params.require(:user).permit(:user_name, :email, :password)
+    end
+
+    def sort_column
+      User.column_names.include?(params[:sort]) ? params[:sort] : 'id'
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
     end
   end
 end
