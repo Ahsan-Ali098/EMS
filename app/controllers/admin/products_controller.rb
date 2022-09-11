ae5d61e1d6516bd0086ee94ff15f9c410716dd54
Ahsan-Ali098 <ahsansaleem098@gmail.com>
2022-09-11 12:45:13 +0500

    Adding Products Controller

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Product Controller
+module Admin
+  # Class for ProductsController
+  class ProductsController < ApplicationController
+    def index; end
+  end
+end
