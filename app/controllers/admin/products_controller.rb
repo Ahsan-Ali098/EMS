3dc8fdb505d31ebecb7b05ac2016defe03194e5d
Ahsan-Ali098 <ahsansaleem098@gmail.com>
2022-09-11 15:42:04 +0500

    CRUD operations for Product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -4,6 +4,48 @@
 module Admin
   # Class for ProductsController
   class ProductsController < ApplicationController
-    def index; end
+    before_action :find_product, only: %i[show update edit destroy]
+    def index
+      @products = Product.all.page(params[:page])
+    end
+    
+    def new
+      @product = Product.new
+    end
+
+    def create
+      @product = Product.new(product_params)
+      if @product.save
+        redirect_to @product
+      else
+        render 'new'
+      end
+    end
+
+    def show; end
+
+    def update
+      if @product.update(product_params)
+        redirect_to([:admin, @product])
+      else
+        render 'edit'
+      end
+    end
+
+    def edit; end
+
+    def destroy
+      @product.destroy
+      redirect_to admin_products_path
+    end
+
+    private
+    def find_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:title, :price, :description, :status)
+    end
   end
 end
