935674d4b87523f85f40860b37aaa4f43616034b
Ahsan-Ali098 <ahsansaleem098@gmail.com>
2022-09-13 15:28:37 +0500

    Adding role in users

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_913_052_254) do # rubocop:disable Metrics/BlockLength
+ActiveRecord::Schema.define(version: 20_220_913_102_418) do # rubocop:disable Metrics/BlockLength
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20_220_913_052_254) do # rubocop:disable Me
     t.string 'unconfirmed_email'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.integer 'role', default: 0
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
