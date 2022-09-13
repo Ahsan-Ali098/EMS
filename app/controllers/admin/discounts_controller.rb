1486bb6fa28ffe5466148644106fea5d0b316ced
Ahsan-Ali098 <ahsansaleem098@gmail.com>
2022-09-13 08:17:47 +0500

    Added Sorting/serarching/Export feature to discount

--- app/controllers/admin/discounts_controller.rb
+++ app/controllers/admin/discounts_controller.rb
@@ -5,9 +5,22 @@ module Admin
   # Class for DiscountsController
   class DiscountsController < ApplicationController
     before_action :set_discount, only: %i[show update edit destroy]
+    helper_method :sort_column, :sort_direction
 
     def index
-      @discounts = Discount.order(name: :desc)
+      per_page = params[:page]
+      search_param = params[:search]
+      search(search_param, per_page)
+      respond_to do |format|
+        format.html
+        format.csv do
+          send_data ExportService::DiscountExport.new(Discount.all).to_csv, filename: "discountsinfo-#{Date.today}.csv"
+        end
+      end
+    end
+
+    def sort_param
+      "#{sort_column} #{sort_direction}"
     end
 
     def new
@@ -42,12 +55,30 @@ module Admin
 
     private
 
+    def search(search_param, per_page)
+      @discounts = if search_param.present?
+                     Discount.search(search_param)
+                             .page(per_page)
+                   else
+                     Discount.all.page(per_page).order(sort_param)
+                   end
+      @discounts.page(per_page)
+    end
+
     def set_discount
       @discount = Discount.find(params[:id])
     end
 
     def discount_params
       params.require(:discount).permit(:name, :price, :products_id)
+    end
+
+    def sort_column
+      Discount.column_names.include?(params[:sort]) ? params[:sort] : 'id'
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
     end
   end
 end
--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,5 +2,13 @@
 
 # discount model file
 class Discount < ApplicationRecord
-  has_many :products
+  paginates_per 5
+  has_many :product
+
+  def self.search(search)
+    Discount.where('cast(id as text) LIKE :value or
+    lower(discounts.name) LIKE :value or
+    cast(discounts.price as text) LIKE :value ',
+                   value: "%#{search.downcase}%")
+  end
 end
