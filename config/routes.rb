b3a079af61ecde1b210a924d4bda728e8484b869
Ahsan-Ali098 <ahsansaleem098@gmail.com>
2022-09-15 17:45:42 +0500

    Adusted routes for order,cart,registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,19 @@
 
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   namespace :admin do
     resources :users
     resources :products
     resources :categories
     resources :discounts
+    resources :orders, only: %i[index show]
   end
 
   namespace :user do
     resources :products, only: [:index]
     resources :order_items, only: %i[create new destroy show]
-    resource  :carts, only: %i[show destroy]
+    resources :carts, only: %i[show destroy]
     resources :orders
   end
   post 'order_items/:id/add' => 'order_items#add_quantity', as: 'order_item_add'
