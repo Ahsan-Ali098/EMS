2df68f05b34fbdef3c8c1f63dc5216980f67daaa
Ahsan-Ali098 <ahsansaleem098@gmail.com>
2022-09-15 17:44:15 +0500

    Empty cart on checkout

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,6 +4,7 @@
 class Cart < ApplicationRecord
   has_many :order_items, dependent: :destroy
   has_many :products, through: :order_items
+  belongs_to :user
 
   def sub_total
     sum = 0
@@ -11,5 +12,9 @@ class Cart < ApplicationRecord
       sum += order_item.total_price
     end
     sum
+  end
+
+  def empty
+    order_items.destroy_all
   end
 end
