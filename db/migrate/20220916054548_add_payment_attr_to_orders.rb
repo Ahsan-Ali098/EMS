2c5135d8b04f11b5e73f3c52e74a6d8d11d00ba9
Ahsan-Ali098 <ahsansaleem098@gmail.com>
2022-09-17 17:28:58 +0500

    Added payment method in order

--- db/migrate/20220916054548_add_payment_attr_to_orders.rb
+++ db/migrate/20220916054548_add_payment_attr_to_orders.rb
@@ -1,5 +1,5 @@
 class AddPaymentAttrToOrders < ActiveRecord::Migration[5.2]
   def change
-    add_column :orders, :payment, :integer,deafult: 0
+    add_column :orders, :payment, :integer, deafult: 0
   end
 end
