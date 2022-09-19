d3c2b36f0e34cd334df3db6aad78960c7ebb3d43
Ahsan-Ali098 <ahsansaleem098@gmail.com>
2022-09-19 11:21:10 +0500

    Empty cart Feature

--- app/controllers/user/carts_controller.rb
+++ app/controllers/user/carts_controller.rb
@@ -5,15 +5,14 @@ class User
   # Carts
   class CartsController < ApplicationController
     before_action :current_cart
+
     def show
       @cart = @current_cart
     end
 
     def destroy
-      @cart = @current_cart
-      @cart.destroy
-      session[:cart_id] = nil
-      redirect_to root_path
+      @current_cart.empty
+      redirect_to user_products_path
     end
   end
 end
